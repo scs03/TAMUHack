95dfaa1a816887a0ed03f9cf7f716cdbe496e82e
srutiswathi <karthikeyansruti@gmail.com>
2025-01-25 17:34:15 -0600

    fixed mui

--- tamuhack/app/page.tsx
+++ tamuhack/app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { Card } from "@mui/material";
-import Navbar from "@/components/navbar";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -11,7 +9,7 @@ export default function Home() {
     //Mobile Sign in Page
     <div className="flex flex-col h-screen">
       <div className="flex flex-col items-center justify-center h-full">
-        <Card className="w-80 p-4">
+        <div className="w-80 p-4">
           <h1 className="text-2xl font-bold">Sign in</h1>
           <input
             className="w-full mt-4 p-2 border border-gray-300 rounded"
@@ -26,7 +24,7 @@ export default function Home() {
           >
             Sign in
           </button>
-        </Card>
+        </div>
       </div>
     </div>
   );
