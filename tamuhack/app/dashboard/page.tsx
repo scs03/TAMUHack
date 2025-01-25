b304c42fd479149d770bb63fe618fa33d6e11822
Sriram Sendhil <sriramsendhil@gmail.com>
2025-01-25 17:20:02 -0600

    lots of updates

---
+++ tamuhack/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '@/components/navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+
+        <div className=''>
+
+        </div>
+
+        <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Dashboard
